d05a242c550ed2499897a01f4a852041340aae82
nbaishev <nbaishev@gmail.com>
2023-05-31 19:27:04 +0600

    deploy 4

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'djoser',
     'django_filters',
     'ingredients.apps.IngredientsConfig',
